82c16311fd38c76da178f182b143bbecb8549286
Ângelo Daher <angelogabrielangelo890@gmail.com>
2026-04-13 12:16:42 -0300

    Add wall area calculation and paint requirement

---
+++ ex python/Exercícios 0 a 100/ex011.py
@@ -0,0 +1,5 @@
+lar = float(input('Digite a largura da parede: '))
+alt = float(input('Digite a Altura da parede: '))
+area = lar * alt
+tinta = area / 2
+print(f'A área de sua parede é de {area}, cada litro de tinta pinta 2 metros, então serão necessários {tinta} litros de tinta!')
