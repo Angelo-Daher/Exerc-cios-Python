1828a1c253961909647cd5f4bcfdc37e61f26625
Ângelo Daher <angelogabrielangelo890@gmail.com>
2026-04-19 14:51:35 -0300

    Add pricing calculation for travel distance

---
+++ ex python/Exercícios 0 a 100/ex031.py
@@ -0,0 +1,6 @@
+km = int(input('Qual a distancia da viagem? '))
+if km <= 200:
+    print(f'Sua viagem sairá no valor de {km * 0.50} reais.')
+else:
+    print(f'Sua viagem sairá no valor de {km * 0.45} reais.'
+          
