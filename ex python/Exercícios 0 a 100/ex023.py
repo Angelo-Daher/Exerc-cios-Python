f8310ee1c599b9c194b55145e245ce20c8c59c19
Ângelo Daher <angelogabrielangelo890@gmail.com>
2026-04-19 01:28:22 -0300

    Add number analysis script for units, tens, hundreds

---
+++ ex python/Exercícios 0 a 100/ex023.py
@@ -0,0 +1,11 @@
+num = int(input('Digite um número: '))
+u = num // 1 % 10
+d = num // 10 % 10
+c = num // 100 % 10
+m = num // 1000 % 10
+
+print(f'Analisando o número {num}:')
+print(f'Sua unidade é: {u}')
+print(f'Sua dezena: {d}')
+print(f'Sua centena: {c}')
+print(f'Sua milhar: {m}')
