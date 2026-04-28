35646b5102f1b676c2d2af18bd0ba61acb3cb655
Ângelo Daher <angelogabrielangelo890@gmail.com>
2026-04-28 13:20:39 -0300

    Add gender input validation in ex057.py

---
+++ ex python/Exercícios 0 a 100/ex057.py
@@ -0,0 +1,7 @@
+sexo = str(input("Qual seu sexo [F/M]: ")).lower()
+while sexo != 'f' and sexo != 'm':
+  print('Escolha somente F ou M!')
+  sexo = str(input("Qual seu sexo [F/M]: ")).lower()
+  
+print('Vamos prosseguir!')
+
