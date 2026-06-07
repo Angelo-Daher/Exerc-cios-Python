776bf270ee01f0381ff987779124c013b111ea83
Ângelo Daher <angelogabrielangelo890@gmail.com>
2026-06-07 16:53:57 -0300

    Organização de código

--- ex python/Exercícios 0 a 100/ex072.py
+++ ex python/Exercícios 0 a 100/ex072.py
@@ -1,6 +1,6 @@
 numeros = ('zero','um','dois','tres','quatro','cinco','seis','sete','oito','nove','dez','onze','doze','treze','quatorze','quinze','dezesseis','dezessete','dezoito','dezenove','vinte')
 
-num = int(input('Digite apenas de 1 a 20: '))
-while num < 1 or num > 20:
-    num = int(input('Tente Novamente! Digite apenas de 1 a 20: '))
+num = int(input('Digite apenas de 0 a 20: '))
+while num not in range(0,21):
+    num = int(input('Tente novamente. Digite apenas de 0 a 20: '))
 print(f'Voce escolheu o número {numeros[num]}')
