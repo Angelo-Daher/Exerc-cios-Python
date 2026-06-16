816aa5c172d2360ef236d1eb7d2e91143b501a3c
Ângelo Daher <angelogabrielangelo890@gmail.com>
2026-06-16 20:16:36 -0300

    adiciona busca de maior e menor valor na lista

--- ex python/Exercícios 0 a 100/ex078.py
+++ ex python/Exercícios 0 a 100/ex078.py
@@ -2,8 +2,8 @@ valores = []
 for contagem in range(0 , 5):
     valores.append(int(input('Digite um valor: ')))
 
-for indice, o_que_tem_no_indice_tal in enumerate(valores):
-    print(f"Na posição {indice} temos o valor {o_que_tem_no_indice_tal}")
+for indice, valor in enumerate(valores):
+    print(f"Na posição {indice} temos o valor {valor}")
 
 print(f'O maior valor digitado foi {max(valores)} na posição {valores.index(max(valores))}!')
 print(f'O menor valor digitado foi {min(valores)} na posiçaõ {valores.index(min(valores))}!')
