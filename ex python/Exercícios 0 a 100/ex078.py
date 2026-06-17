f3f681683c354551b8dd9f7aec69fa1ad252b2c3
Ângelo Daher <angelogabrielangelo890@gmail.com>
2026-06-16 21:07:03 -0300

    identifica maior e menor valor e suas posições

--- ex python/Exercícios 0 a 100/ex078.py
+++ ex python/Exercícios 0 a 100/ex078.py
@@ -2,8 +2,29 @@ valores = []
 for contagem in range(0 , 5):
     valores.append(int(input('Digite um valor: ')))
 
-for indice, valor in enumerate(valores):
-    print(f"Na posição {indice} temos o valor {valor}")
 
-print(f'O maior valor digitado foi {max(valores)} na posição {valores.index(max(valores))}!')
-print(f'O menor valor digitado foi {min(valores)} na posiçaõ {valores.index(min(valores))}!')
+maior = max(valores)
+menor = min(valores)
+
+
+print(valores)
+print(f'O maior valor digitado foi {maior} nas posições ',end='')
+
+
+for indice, valor in enumerate(valores):
+
+    if valor == maior:
+        print(indice,end='... ')
+
+
+print(f'O menor valor digitado foi {menor} nas posições ',end=' ')
+
+for indice, valor in enumerate(valores):
+
+    if valor == menor:
+        print(indice,end='... ')
+        
+
+
+#print(f'O maior valor digitado foi {max(valores)} nas posições {len(valores.index(max(valores)))}!') - SERVEM PORÉM, só pegam a primeira ocorrencia 
+#print(f'O menor valor digitado foi {min(valores)} nas posições {valores.index(min(valores))}!')
